5278c73bb421f1cac0b80b76a9773ad6330d6e42
Артём <snowboard_team@mail.ru>
2022-07-13 14:08:22 +0600

    finish

--- part1/practice/bad_smell_call_chain/main.py
+++ part1/practice/bad_smell_call_chain/main.py
@@ -2,43 +2,20 @@
 # оперировали внутренним состоянием, 
 # а не использовали цепочку вызовов объектов
 
-class Room:
-    def get_name(self):
-        return 42
-
-
-class Street:
-    def get_room(self) -> Room:
-        return Room()
-
-
-class City:
-    def get_street(self) -> Street:
-        return Street()
-
-    def population(self):
-        return 100500
-
-
-class Country:
-    def get_city(self) -> City:
-        return City()
-
-
-class Planet:
-    def get_contry(self) -> Country:
-        return Country()
-
 
 class Person:
-    def __init__(self):
-        self.planet = Planet()
+    def __init__(self, city_population, room_num, street_name, city_name, country_name):
+        self.city_population = city_population
+        self.room_num = room_num
+        self.street = street_name
+        self.city = city_name
+        self.country = country_name
 
     def get_person_room(self):
-        return self.planet.get_contry().get_city().get_street().get_room().get_name()
+        return self.room_num
 
     def get_city_population(self):
-        return self.planet.get_contry().get_city().population()
+        return self.city_population
 
 
 # TODO после выполнения задания попробуйте
